e4e2d991b7fad5b7ddedd3f9baaeadcd7943c4b1
wjin17 <willjin1796@gmail.com>
2025-03-13 01:41:01 -0700

    update mdx components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  site: "https://example.com",
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "everforest-dark" },
+    }),
+    sitemap(),
+    react(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
