9fda481e557c2c1e5a78179a898af70f3cbe053c
wjin17 <willjin1796@gmail.com>
2025-03-16 12:32:58 -0700

    add gh pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://wjin17.github.io",
   integrations: [
     mdx({
       syntaxHighlight: "shiki",
